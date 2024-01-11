e001c52f38f2ab95803133d7b244c966d503dc13
Elmo Lee <>
2024-01-11 14:11:25 +0800

    Create Framework

---
+++ LeeSwiftComponents/LeeSwiftComponents/LeeSwiftComponents.h
@@ -0,0 +1,18 @@
+//
+//  LeeSwiftComponents.h
+//  LeeSwiftComponents
+//
+//  Created by KWUN LOK LEE on 11/1/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LeeSwiftComponents.
+FOUNDATION_EXPORT double LeeSwiftComponentsVersionNumber;
+
+//! Project version string for LeeSwiftComponents.
+FOUNDATION_EXPORT const unsigned char LeeSwiftComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LeeSwiftComponents/PublicHeader.h>
+
+
