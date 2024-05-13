6340e37686d9ee13f4a46a38524b991a0bc47378
Elmo Lee <>
2024-05-13 13:49:06 +0800

    Add the function that calculate distance date between today and your provide date.

--- LeeSwiftComponents/LeeSwiftComponents/Extension/DateFormatter.swift
+++ LeeSwiftComponents/LeeSwiftComponents/Extension/DateFormatter.swift
@@ -6,7 +6,7 @@
 //
 
 public extension DateFormatter {
-  func dateFormat(string: String, from: String? = "yyyy-MM-dd HH:mm:ss", changeTo: String? = "yyyy/MM/dd", locale: Locale? = Locale(identifier: "en_US_POSIX"), timeZone: TimeZone? = TimeZone(abbreviation: "GMT")) -> String {
+  func dateFormat(string: String, from: String? = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", changeTo: String? = "yyyy/MM/dd", locale: Locale? = Locale(identifier: "en_US_POSIX"), timeZone: TimeZone? = TimeZone(abbreviation: "UTC")) -> String {
     let dateFormatter = DateFormatter();
     dateFormatter.dateFormat = from;
     dateFormatter.timeZone = timeZone;
@@ -16,5 +16,20 @@ public extension DateFormatter {
       return dateFormatter.string(from: formateDate);
     }
     return string;
+  }
+  
+  func distanceNow(string: String, from: String? = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", locale: Locale? = Locale(identifier: "en_US_POSIX"), timeZone: TimeZone? = TimeZone(abbreviation: "UTC")) -> Int? {
+    let dateFormatter = DateFormatter();
+    dateFormatter.dateFormat = from;
+    dateFormatter.timeZone = timeZone;
+    dateFormatter.locale =  locale;
+    
+    if let formateDate = dateFormatter.date(from: string) {
+      let now = Date();
+      let distance = now.distance(to: formateDate);
+
+      return Int(distance);
+    }
+    return nil;
   }
 }
